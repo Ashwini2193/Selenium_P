db493fba8fd0c45305f12804874a0de0a8caeb42
Ashwini2193 <ashwini-venkatasubbarao-mutte.mutte@capgemini.com>
2026-08-10 12:19:07 +0530

    window handling

---
+++ src/test/java/windowHandling.java
@@ -0,0 +1,29 @@
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class windowHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.tutorialspoint.com/selenium/practice/browser-windows.php");
+WebElement btn=driver.findElement(By.xpath("//button[@title='New Tab']"));
+btn.click();
+String parent=driver.getWindowHandle();
+Set<String> allwindows=driver.getWindowHandles();
+for(String all:allwindows) {
+	driver.switchTo().window(all);
+	String allTitle=driver.getTitle();
+	System.out.println(allTitle);
+	
+}
+driver.switchTo().window(parent);
+String parentTile=driver.getTitle();
+System.out.println(parentTile);
+	}
+
+}
